5f5d52530f9fac791ee59357397c8607541e14a9
ikekyo <ike.kyo0802@gmail.com>
2020-02-23 16:54:47 +0900

    add google-tts-api key.js

---
+++ google-tts-api/lib/key.js
@@ -0,0 +1,25 @@
+var fetch = require('isomorphic-fetch');
+var host = 'https://translate.google.com';
+
+/**
+ * Get Key from https://translate.google.com
+ *
+ * @param   {Number!} timeout  default is 10000ms
+ * @return  Promise(key: String)
+ */
+module.exports = function (timeout) {
+  return fetch(host, {
+    timeout: timeout || 10 * 1000
+  })
+  .then(function (res) {
+    if (res.status !== 200) {
+      throw new Error('request to ' + host + ' failed, status code = ' + res.status + ' (' + res.statusText + ')');
+    }
+    return res.text();
+  })
+  .then(function (html) {
+    var match = html.match("tkk:'(\\d+.\\d+)',"); //TKK = '436001.3073172371'
+    if (!match) throw new Error('get key failed from google');
+    return match[1];
+  });
+};
